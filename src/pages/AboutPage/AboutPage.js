fa1b8b13bea0269577aeee9018709f86e8965021
Clara Mendes <claraplanejamento@gmail.com>
2021-11-22 09:20:48 -0300

    :truck: Changes aboutPage styles to separate file

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled, { keyframes, ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components";
 import { DarkTheme } from "../../components/Themes";
 
 import LogoComponent from "../../subComponents/LogoComponent";
@@ -8,51 +8,7 @@ import PowerButton from "../../subComponents/PowerButton";
 import ParticleComponent from "../../subComponents/ParticleComponent";
 import BigTitle from "../../subComponents/BigTitlte";
 import clara from "../../assets/Images/clara.jpeg";
-
-const Box = styled.div`
-  background-color: #011638;
-  width: 100vw;
-  height: 100vh;
-  position: relative;
-  overflow: hidden;
-`;
-const float = keyframes`
-0% { transform: translateY(-10px) }
-50% { transform: translateY(15px) translateX(15px) }
-100% { transform: translateY(-10px) translateX(-15px) }
-
-`;
-const Spaceman = styled.div`
-  position: absolute;
-  top: 10%;
-  right: 5%;
-  width: 20vw;
-  animation: ${float} 4s ease infinite;
-  img {
-    width: 100%;
-    height: auto;
-    border-radius: 50%;
-  }
-`;
-const Main = styled.div`
-  border: 2px solid ${(props) => props.theme.text};
-  color: ${(props) => props.theme.text};
-  padding: 2rem;
-  width: 50vw;
-  height: 60vh;
-  z-index: 3;
-  line-height: 1.5;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: calc(0.6rem + 1vw);
-  backdrop-filter: blur(4px);
-
-  position: absolute;
-  left: calc(5rem + 5vw);
-  top: 10rem;
-  font-family: 'Courier New', Courier, monospace;
-`;
+import { Box, Spaceman, Main } from "./styles";
 
 const AboutPage = () => {
   return (
@@ -67,15 +23,15 @@ const AboutPage = () => {
           <img src={clara} alt="spaceman" />
         </Spaceman>
         <Main>
-          I'm a web developer located in Brazil. I am currently working at Ribon App,
-          a startup focused on enabling donations to NGOs.
+          I'm a web developer located in Brazil. I am currently working at Ribon
+          App, a startup focused on enabling donations to NGOs.
           <br /> <br />
-          I started to study computer science in 2019 and focused a lot on front-end 
-          development so far, with React.js being my main framework of choice. In spite of
-          that, I also have great interest in back-end development, especially with Ruby on Rails.
-          <br /> <br />
-          I love to read and watch movies, and right now I'm learning more about algorithms, data 
-          structures and good practices.
+          I started to study computer science in 2019 and focused a lot on
+          front-end development so far, with React.js being my main framework of
+          choice. In spite of that, I also have great interest in back-end
+          development, especially with Ruby on Rails.
+          <br /> <br />I love to read and watch movies, and right now I'm
+          learning more about algorithms, data structures and good practices.
         </Main>
 
         <BigTitle text="ABOUT" top="10%" left="5%" />
---
+++ src/pages/AboutPage/styles.js
@@ -0,0 +1,48 @@
+import styled, { keyframes, ThemeProvider } from "styled-components";
+
+export const Box = styled.div`
+  background-color: #011638;
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+`;
+
+export const float = keyframes`
+0% { transform: translateY(-10px) }
+50% { transform: translateY(15px) translateX(15px) }
+100% { transform: translateY(-10px) translateX(-15px) }
+`;
+
+export const Spaceman = styled.div`
+  position: absolute;
+  top: 10%;
+  right: 5%;
+  width: 20vw;
+  animation: ${float} 4s ease infinite;
+  img {
+    width: 100%;
+    height: auto;
+    border-radius: 50%;
+  }
+`;
+
+export const Main = styled.div`
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  padding: 2rem;
+  width: 50vw;
+  height: 60vh;
+  z-index: 3;
+  line-height: 1.5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(0.6rem + 1vw);
+  backdrop-filter: blur(4px);
+
+  position: absolute;
+  left: calc(5rem + 5vw);
+  top: 10rem;
+  font-family: "Courier New", Courier, monospace;
+`;
