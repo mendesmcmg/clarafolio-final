5f30b1834fc46d383854ed6a929bad8a2c94d8ce
Clara Mendes <claraplanejamento@gmail.com>
2021-11-22 09:35:24 -0300

    :truck: changes main styles to separate file

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,108 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  background: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+
+  position: relative;
+
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: "Karla", sans-serif;
+    font-weight: 500;
+  }
+`;
+
+export const Container = styled.div`
+  padding: 2rem;
+`;
+
+export const Contact = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 2rem;
+  right: calc(1rem + 2vw);
+  text-decoration: none;
+  z-index: 1;
+`;
+export const BLOG = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 50%;
+  right: calc(1rem + 2vw);
+  transform: rotate(90deg) translate(-50%, -50%);
+  text-decoration: none;
+  z-index: 1;
+`;
+export const WORK = styled(NavLink)`
+  color: ${(props) => (props.click ? props.theme.body : props.theme.text)};
+
+  position: absolute;
+  top: 50%;
+  left: calc(1rem + 2vw);
+  transform: translate(-50%, -50%) rotate(-90deg);
+  text-decoration: none;
+  z-index: 1;
+`;
+
+export const BottomBar = styled.div`
+  position: absolute;
+  bottom: 1rem;
+  left: 0;
+  right: 0;
+  width: 100%;
+
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+export const ABOUT = styled(NavLink)`
+  color: ${(props) => (props.click ? props.theme.body : props.theme.text)};
+  text-decoration: none;
+  z-index: 1;
+`;
+export const SKILLS = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  text-decoration: none;
+  z-index: 1;
+`;
+
+export const Center = styled.button`
+  position: absolute;
+  top: ${(props) => (props.click ? "85%" : "50%")};
+  left: ${(props) => (props.click ? "92%" : "50%")};
+  transform: translate(-50%, -50%);
+  border: none;
+  outline: none;
+  background-color: transparent;
+  cursor: pointer;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  transition: all 1s ease;
+
+  & > :last-child {
+    display: ${(props) => (props.click ? "none" : "inline-block")};
+    padding-top: 1rem;
+  }
+`;
+
+export const DarkDiv = styled.div`
+  position: absolute;
+  top: 0;
+  background-color: #011638;
+  bottom: 0;
+  right: 50%;
+  width: ${(props) => (props.click ? "50%" : "0%")};
+  height: ${(props) => (props.click ? "100%" : "0%")};
+  z-index: 1;
+  transition: height 0.5s ease, width 1s ease 0.5s;
+`;
