4b7140a612adc7a5b2fc7c9f59554ccbe7389cc0
Clara Mendes <claraplanejamento@gmail.com>
2021-11-22 09:48:28 -0300

    :truck: changes workpage styles to separate file

---
+++ src/pages/WorkPage/styles.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+export const Box = styled.div`
+  background-color: ${(props) => props.theme.body};
+
+  height: 400vh;
+  position: relative;
+  display: flex;
+  align-items: center;
+`;
+
+export const Main = styled(motion.ul)`
+  position: fixed;
+  top: 12rem;
+  left: calc(10rem + 15vw);
+  height: 40vh;
+  display: flex;
+
+  color: white;
+`;
+
+export const Rotate = styled.span`
+  display: block;
+  position: fixed;
+  right: 1rem;
+  bottom: 1rem;
+  width: 80px;
+  height: 80px;
+  z-index: 1;
+`;
+
+// Framer-motion Configuration
+export const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+
+    transition: {
+      staggerChildren: 0.5,
+      duration: 0.5,
+    },
+  },
+};
