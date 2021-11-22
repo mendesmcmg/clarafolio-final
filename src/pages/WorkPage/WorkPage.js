1da690e452c350332508c22dc64c85915bbe0b91
Clara Mendes <claraplanejamento@gmail.com>
2021-11-22 09:48:49 -0300

    :truck: changes workpage styles to separate file

--- src/pages/WorkPage/WorkPage.js
+++ src/pages/WorkPage/WorkPage.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef } from "react";
-import styled, { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components";
 import { DarkTheme } from "../../components/Themes";
-import { motion } from "framer-motion";
 
 import LogoComponent from "../../subComponents/LogoComponent";
 import SocialIcons from "../../subComponents/SocialIcons";
@@ -11,47 +10,7 @@ import { Work } from "../../data/WorkData";
 import Card from "../../subComponents/Card";
 import MainAnimation from "../../assets/animations/MainAnimations";
 import BigTitle from "../../subComponents/BigTitle";
-
-const Box = styled.div`
-  background-color: ${(props) => props.theme.body};
-
-  height: 400vh;
-  position: relative;
-  display: flex;
-  align-items: center;
-`;
-
-const Main = styled(motion.ul)`
-  position: fixed;
-  top: 12rem;
-  left: calc(10rem + 15vw);
-  height: 40vh;
-  display: flex;
-
-  color: white;
-`;
-const Rotate = styled.span`
-  display: block;
-  position: fixed;
-  right: 1rem;
-  bottom: 1rem;
-  width: 80px;
-  height: 80px;
-  z-index: 1;
-`;
-
-// Framer-motion Configuration
-const container = {
-  hidden: { opacity: 0 },
-  show: {
-    opacity: 1,
-
-    transition: {
-      staggerChildren: 0.5,
-      duration: 0.5,
-    },
-  },
-};
+import { Box, Main, container, Rotate } from "./styles";
 
 const WorkPage = () => {
   const ref = useRef(null);
